d4bebb546563f70ce63129480ccd8c887b79643b
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-04-03 14:41:09 +0800

    Update LIKE operator to use Postgres-specific SQL syntax for production environment

--- app/controllers/admin/employee_searches_controller.rb
+++ app/controllers/admin/employee_searches_controller.rb
@@ -2,8 +2,13 @@ class Admin::EmployeeSearchesController < Admin::ApplicationController
   
   def index
     if params[:q].present?
-      query = "last_name like ?"
-      value = "#{params[:q]}%"
+      if Rails.env.development?
+        query = "last_name like ?"
+      elsif Rails.env.production?
+        query = "last_name ilike ?"
+      end
+
+      value = "%#{params[:q]}%"
       @searched_employees = Employee.includes(:rank).where(query, value)
     else
       @searched_employees = Employee.includes(:rank).all
