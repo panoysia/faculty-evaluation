a9b98f80117bfadd2dd4b2efacefd7d851feea63
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-04-03 14:31:50 +0800

    Update LIKE operator in Employee search function

--- app/controllers/admin/employee_searches_controller.rb
+++ app/controllers/admin/employee_searches_controller.rb
@@ -3,7 +3,7 @@ class Admin::EmployeeSearchesController < Admin::ApplicationController
   def index
     if params[:q].present?
       query = "last_name like ?"
-      value = "%#{params[:q]}%"
+      value = "#{params[:q]}%"
       @searched_employees = Employee.includes(:rank).where(query, value)
     else
       @searched_employees = Employee.includes(:rank).all
