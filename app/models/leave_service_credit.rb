b321e665f73820f2b2672b41fea6d41630925139
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-04-05 01:36:00 +0800

    Update LeaveServiceCredit resources again. Mostly functioning..

--- app/models/leave_service_credit.rb
+++ app/models/leave_service_credit.rb
@@ -3,14 +3,27 @@ class LeaveServiceCredit < ActiveRecord::Base
   belongs_to :academic_year
 
   validates :employee_id, :academic_year_id, :valid_at, presence: true
+  validate :ensure_within_academic_year
 
   before_save :set_expire_at_date
   
 
   private
 
+  def ensure_within_academic_year
+    unless (academic_year.start_at..academic_year.end_at).include?(valid_at.to_date)
+      errors[:base] << "Ensure that the Valid At date must be within the date range of the selected academic year period."
+    end
+  end
+
+  # def correct_date_range
+  #   unless end_at > start_at
+  #     errors[:base] << "Invalid date range.\n'End at' date value must be greater than 'Start at' date value."
+  #   end
+  # end
+
   def set_expire_at_date
     self[:expire_at] = valid_at + 1.year
   end
-  
+
 end
