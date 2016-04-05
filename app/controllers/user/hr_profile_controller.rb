5ab9789507dbf0b6092cc6c2b12b93cf85bc7207
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-04-05 11:09:40 +0800

    Update Employee HR Profile for User view

--- app/controllers/user/hr_profile_controller.rb
+++ app/controllers/user/hr_profile_controller.rb
@@ -4,6 +4,8 @@ class User::HRProfileController < User::ApplicationController
     # find the polymorphic account of the user/employee
     #   current_user.account
     #@profile = Employee.find(current_user)
+
+    @profile = @current_user.account
   end
 
 
