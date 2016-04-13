50822ce0ee64c9bc2cdfba78c122fc5330f4ca0c
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-04-14 04:37:56 +0800

    Slight changes for Heroku repo 2

--- db/migrate/20160411173411_change_type_field_to_integer_in_events.rb
+++ db/migrate/20160411173411_change_type_field_to_integer_in_events.rb
@@ -1,5 +1,6 @@
 class ChangeTypeFieldToIntegerInEvents < ActiveRecord::Migration
   def change
+    # uncomment:
     # change_column :events, :type, :integer, null: false
     rename_column :events, :type, :category 
   end
--- db/migrate/20160413004607_remove_columns_in_qce_ratings.rb
+++ db/migrate/20160413004607_remove_columns_in_qce_ratings.rb
@@ -3,6 +3,7 @@ class RemoveColumnsInQCERatings < ActiveRecord::Migration
     remove_column :qce_ratings, :academic_year_id, :integer
     remove_column :qce_ratings, :semeseter, :integer
     remove_column :qce_ratings, :nbc_id, :integer
-    remove_column :qce_ratings, :rating_period_id, :integer
+    # uncomment:
+    #remove_column :qce_ratings, :rating_period_id, :integer
   end
 end
