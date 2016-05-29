04511472ec3101f8b4a753acd1a0bde6096a65ad
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-05-29 19:38:15 +0800

    Update migration files for Heroku

--- db/migrate/20160522193118_create_rating_notifications.rb
+++ db/migrate/20160522193118_create_rating_notifications.rb
@@ -1,12 +1,12 @@
 class CreateRatingNotifications < ActiveRecord::Migration
   def change
-    create_table :rating_notifications do |t|
-      t.references :owner, index: true, foreign_key: true, null: false
-      t.references :evaluator, polymorphic: true, index: true, null: false
-      t.references :rating, polymorphic: true, index: true, null: false
-      t.integer :status, null: false
+    # create_table :rating_notifications do |t|
+    #   t.references :owner, index: true, foreign_key: true, null: false
+    #   t.references :evaluator, polymorphic: true, index: true, null: false
+    #   t.references :rating, polymorphic: true, index: true, null: false
+    #   t.integer :status, null: false
 
-      t.timestamps null: false
-    end
+    #   t.timestamps null: false
+    # end
   end
 end
