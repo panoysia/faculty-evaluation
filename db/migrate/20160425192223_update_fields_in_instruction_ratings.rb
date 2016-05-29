c020fa4f55d4e7bfedaa00fce35febfbd719b272
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-05-29 19:28:53 +0800

    Update migration files for Heroku

--- db/migrate/20160425192223_update_fields_in_instruction_ratings.rb
+++ db/migrate/20160425192223_update_fields_in_instruction_ratings.rb
@@ -1,5 +1,5 @@
 class UpdateFieldsInInstructionRatings < ActiveRecord::Migration
   def change
-    change_column_null :qce_instruction_ratings, :evaluator_id, false
+    change_column_null :qce_instruction_ratings, :evaluator_id, true
   end
 end
