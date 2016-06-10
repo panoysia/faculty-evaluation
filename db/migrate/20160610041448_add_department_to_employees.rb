c952744c7108b49bcd8994ada1333265b59391d1
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-06-10 12:51:58 +0800

    Update migration for Heroku

--- db/migrate/20160610041448_add_department_to_employees.rb
+++ db/migrate/20160610041448_add_department_to_employees.rb
@@ -1,5 +1,8 @@
 class AddDepartmentToEmployees < ActiveRecord::Migration
   def change
-    add_reference :employees, :department, index: true, foreign_key: true, null: false, default: 0
+    # add_reference :employees, :department, index: true, foreign_key: true, null: false , default: 0
+
+    add_reference :employees, :department, index: true, foreign_key: true
+
   end
 end
