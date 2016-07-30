002061b1b154005b2a4968520eb6a267e2d3e260
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2016-07-31 03:53:26 +0800

    Modify a previous migration to match Heroku

--- db/migrate/20160724205332_remove_nature_from_employee_innovations.rb
+++ db/migrate/20160724205332_remove_nature_from_employee_innovations.rb
@@ -1,6 +1,10 @@
 class RemoveNatureFromEmployeeInnovations < ActiveRecord::Migration
   def change
     remove_column :employee_innovations, :nature, :integer
-    change_column :employee_innovations, :year_patented, :integer, null: false
+    remove_column :employee_innovations, :year_patented, :date
+    
+    add_column :employee_innovations, :year_patented, :integer
+    change_column_null :employee_innovations, :year_patented, false
+    # change_column :employee_innovations, :year_patented, :integer, null: false
   end
 end
