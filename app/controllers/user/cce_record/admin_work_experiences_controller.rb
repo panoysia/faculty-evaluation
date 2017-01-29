2842e4edc53bc950cd9ec44437bd99a3a4dbab3d
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2017-01-30 01:24:05 +0800

    Add User CCE record for work experiences

--- app/controllers/user/cce_record/admin_work_experiences_controller.rb
+++ app/controllers/user/cce_record/admin_work_experiences_controller.rb
@@ -1,19 +1,60 @@
 class User::CCERecord::AdminWorkExperiencesController < User::ApplicationController
 
+  before_action :set_employee
+  before_action :set_work_experience, only: [:edit, :update, :destroy]
+
+
   def new
+    @work_experience = employee.admin_work_experiences.new
   end
 
   def create
+    @work_experience = employee.admin_work_experiences.
+                          new(experience_params)
+    if @work_experience.save
+      redirect_to cce_record_path,
+        notice: "Administrative work experience record was successfully created."
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @work_experience.update(experience_params)
+      redirect_to cce_record_path,
+        notice: "Administrative work experience record was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @work_experience.destroy
+    redirect_to cce_record_path,
+      notice: "Administrative work experience record was successfully deleted."
+  end
+
+
+  private
+
+
+  def employee
+    @employee = current_user.account
+  end
+
+  alias_method :set_employee, :employee
+
+
+  def set_work_experience
+    @work_experience = employee.admin_work_experiences.find(params[:id])
+  end
+
+  def experience_params
+    params.require(:admin_work_experience).permit(:start_at, :end_at,
+                                                :position, :institution)
   end
 
 end
-  
--- app/controllers/user/cce_record/professional_work_experiences_controller.rb
+++ app/controllers/user/cce_record/professional_work_experiences_controller.rb
@@ -1,18 +1,62 @@
 class User::CCERecord::ProfessionalWorkExperiencesController < User::ApplicationController
 
+  before_action :set_employee
+  before_action :set_work_experience, only: [:edit, :update, :destroy]
+
+
   def new
+    @work_experience = employee.professional_work_experiences.new
   end
 
   def create
+    @work_experience = employee.professional_work_experiences.
+                          new(experience_params)
+    if @work_experience.save
+      redirect_to cce_record_path,
+        notice: "Professional work experience record was successfully created."
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @work_experience.update(experience_params)
+      redirect_to cce_record_path,
+        notice: "Professional work experience record was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @work_experience.destroy
+    redirect_to cce_record_path,
+      notice: "Professional work experience record was successfully deleted."
+  end
+
+
+  private
+
+
+  def employee
+    @employee = current_user.account
+  end
+
+  alias_method :set_employee, :employee
+
+
+  def set_work_experience
+    @work_experience = employee.professional_work_experiences.
+                          find(params[:id])
+  end
+
+
+  def experience_params
+    params.require(:professional_work_experience).
+      permit(:start_at,:end_at, :position, :institution)
   end
 
 end
