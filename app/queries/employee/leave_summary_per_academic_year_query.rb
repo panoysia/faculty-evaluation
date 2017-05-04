6d0ef3c9fb11a96097979a936e1fd2fb965a0077
Elmer A. Sia <pragmatic_panoy@yahoo.com>
2017-05-05 07:41:18 +0800

    Update Leave summary per academic year query object to use Postgres COALESCE

--- app/queries/employee/leave_summary_per_academic_year_query.rb
+++ app/queries/employee/leave_summary_per_academic_year_query.rb
@@ -5,56 +5,98 @@ class Employee::LeaveSummaryPerAcademicYearQuery
   end
   
   def result
-    Leave.connection.select_all(<<-SQL)
-      SELECT
-        ay.id,
-        ay.start_at,
-        ay.end_at,
-        15 AS max_leaves,
-        IFNULL(lsc.service_credits, 0) AS total_credits,
-        IFNULL(leaves.total_days, 0) AS total_days,
-        ((15 - IFNULL(leaves.total_days, 0)) +
-          IFNULL(lsc.service_credits, 0)) AS remaining
-      FROM academic_years ay
-      LEFT JOIN (
-        SELECT
-          academic_year_id,
-          SUM(no_of_days) AS service_credits
-        FROM leave_service_credits
-        WHERE employee_id = #{employee.id}
-        GROUP BY academic_year_id
-      ) lsc
-        ON lsc.academic_year_id = ay.id
-      LEFT JOIN (
-        SELECT
-          academic_year_id,
-          SUM(length) AS total_days
-        FROM leaves
-        WHERE employee_id = #{employee.id}
-        GROUP BY academic_year_id
-      ) leaves
-        ON leaves.academic_year_id = ay.id
-      ORDER BY ay.start_at DESC, ay.end_at DESC;
-    SQL
+    if Rails.env.production?
+      Leave.connection.select_all(sql_for_postgres)
+    elsif Rails.env.development?
+      Leave.connection.select_all(sql_for_sqlite)
+    end
   end
 
 
-  private
+  # private
 
-  attr_reader :employee
+
+  attr_reader :employee, :sql_for_postgres, :sql_for_sqlite
+
+
+  def sql_for_postgres
+  <<-SQL
+    SELECT
+      ay.id,
+      ay.start_at,
+      ay.end_at,
+      15 AS max_leaves,
+      COALESCE(lsc.service_credits, 0) AS total_credits,
+      COALESCE(leaves.total_days, 0) AS total_days,
+      ((15 - COALESCE(leaves.total_days, 0)) +
+        COALESCE(lsc.service_credits, 0)) AS remaining
+    FROM academic_years ay
+    LEFT JOIN (
+      SELECT
+        academic_year_id,
+        SUM(no_of_days) AS service_credits
+      FROM leave_service_credits
+      WHERE employee_id = #{employee.id}
+      GROUP BY academic_year_id
+    ) lsc
+      ON lsc.academic_year_id = ay.id
+    LEFT JOIN (
+      SELECT
+        academic_year_id,
+        SUM(length) AS total_days
+      FROM leaves
+      WHERE employee_id = #{employee.id}
+      GROUP BY academic_year_id
+    ) leaves
+      ON leaves.academic_year_id = ay.id
+    ORDER BY ay.start_at DESC, ay.end_at DESC;
+  SQL
+  end
+
+  def sql_for_sqlite
+  <<-SQL
+    SELECT
+      ay.id,
+      ay.start_at,
+      ay.end_at,
+      15 AS max_leaves,
+      IFNULL(lsc.service_credits, 0) AS total_credits,
+      IFNULL(leaves.total_days, 0) AS total_days,
+      ((15 - IFNULL(leaves.total_days, 0)) +
+        IFNULL(lsc.service_credits, 0)) AS remaining
+    FROM academic_years ay
+    LEFT JOIN (
+      SELECT
+        academic_year_id,
+        SUM(no_of_days) AS service_credits
+      FROM leave_service_credits
+      WHERE employee_id = #{employee.id}
+      GROUP BY academic_year_id
+    ) lsc
+      ON lsc.academic_year_id = ay.id
+    LEFT JOIN (
+      SELECT
+        academic_year_id,
+        SUM(length) AS total_days
+      FROM leaves
+      WHERE employee_id = #{employee.id}
+      GROUP BY academic_year_id
+    ) leaves
+      ON leaves.academic_year_id = ay.id
+    ORDER BY ay.start_at DESC, ay.end_at DESC;
+  SQL
+  end
 
 end
 
 =begin
+  Interchange the term "relation" and "result" in the method that
+  returns data.
 
-  * Interchange the term "relation" and "result" in the method that
-    returns data.
+  relation  => ActiveRecord::Relation
+  result    => ActiveRecord::Result
 
-    # relation  => ActiveRecord::Relation
-    # result    => ActiveRecord::Result
-
-  * Use can also use Model#find_by_sql for custom SQL queries.
-    It returns an array of objects from the model where you called
-    #find_by_sql.
-
+  Use can also use Model#find_by_sql for custom SQL queries.
+  It returns an array of objects from the model where you called
+  #find_by_sql.
 =end
